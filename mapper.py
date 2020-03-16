6c839619816cebe5ac04c7124bb2bcb0607a3983
Robert <rtedwards.eng@gmail.com>
2020-03-16 00:35:46 +0000

    simple choropleth functionality

---
+++ mapper.py
@@ -0,0 +1,34 @@
+import folium
+import geopandas as gpd
+import joblib
+import json
+import os
+import pandas as pd
+import urllib
+
+def choropleth_map(df,columns,geo_data,color,legend):
+    cmap = folium.Map(location=[48, -102], 
+                      tiles='OpenStreetMap',
+                      zoom_start=4,
+                      min_zoom=4,
+                      max_zoom=15)
+
+    folium.Choropleth(geo_data=country_geo,
+                      name='choropleth',
+                      data=df,
+                      columns=['ISO3 Code', 'confirmed'],
+                      key_on='feature.id',
+                      fill_color='YlGn',
+                      fill_opacity=0.7,
+                      line_opacity=0.2,
+                      legend_name='Confirmed Cases',
+                      show=False
+    ).add_to(cmap)
+
+    folium.TileLayer('Stamen Terrain').add_to(cmap)
+    folium.TileLayer('MapQuest Open Aerial', attr="MapQuest Open Aerial").add_to(cmap)
+    folium.TileLayer('cartodbdark_matter').add_to(cmap)
+    folium.TileLayer('stamentoner').add_to(cmap)
+    folium.LayerControl().add_to(cmap)
+
+    return cmap
