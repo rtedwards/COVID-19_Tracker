0a7c66768565b32372f189b22e7182d65da475e7
Robert <rtedwards.eng@gmail.com>
2020-03-16 01:23:01 +0000

    added pulls time_series data and concats into one dataframe

---
+++ preprocesser.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import geopandas as gpd
+import joblib
+import json
+import folium
+import urllib
+import os
+
+def get_data(type=time_series): 
+
+    if type == time_series:
+        TIME_DATA_DIR = '/Users/Berto/Projects/COVID-19_Tracker/COVID-19/csse_covid_19_data/csse_covid_19_time_series'
+        
+        CONFIRMED = os.path.join(TIME_DATA_DIR, "time_series_19-covid-Confirmed.csv")
+        DEATHS = os.path.join(TIME_DATA_DIR, "time_series_19-covid-Deaths.csv")
+        RECOVERED = os.path.join(TIME_DATA_DIR, "time_series_19-covid-Recovered.csv")
+
+        confirmed_df = pd.read_csv(CONFIRMED)
+        deaths_df = pd.read_csv(DEATHS)
+        recovered_df = pd.read_csv(RECOVERED)
+
+        confirmed_df = confirmed_df.melt(id_vars=['Province/State', 'Country/Region', 'Lat', 'Long'],
+                                         var_name='date', 
+                                         value_name='confirmed')
+        deaths_df = deaths_df.melt(id_vars=['Province/State', 'Country/Region', 'Lat', 'Long'],
+                                   var_name='date', 
+                                   value_name='deaths')
+        recovered_df = recovered_df.melt(id_vars=['Province/State', 'Country/Region', 'Lat', 'Long'],
+                                         var_name='date', 
+                                         value_name='recovered')
+
+        df = pd.merge(confirmed_df, deaths_df, 
+                      on=['Province/State', 'Country/Region', 'Lat', 'Long', 'date'],
+                      how='inner')
+        df = pd.merge(df, recovered_df, 
+                      on=['Province/State', 'Country/Region', 'Lat', 'Long', 'date'],
+                      how='inner')
+
+        return df
+
+    elif type == daily:
+        DAILY_DATA_DIR = '/Users/Berto/Projects/COVID-19_Tracker/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports'
+
+        date_list = os.listdir(DAILY_DATA_DIR)
+        date_list.remove('.gitignore')
+        date_list.remove('README.md')
+        date_list.sort()
+
+    else:
+        print("TypeError")
+
