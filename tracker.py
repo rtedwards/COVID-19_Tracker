f3064881cb58aaaf50d555ca3ca11b8a070144b6
Robert <rtedwards.eng@gmail.com>
2020-03-17 09:31:07 +0000

    added date selection box

--- tracker.py
+++ tracker.py
@@ -2,15 +2,24 @@ import pandas as pd
 import geopandas as gpd
 import joblib
 import folium
+import numpy as np
 import os
 import streamlit as st
 from utilities import list_all_tags, is_tag, find_tag
+from preprocesser import load_data
 
-DATA_DIR = '/Users/Berto/Projects/COVID-19_Tracker/COVID-19/csse_covid_19_data/csse_covid_19_time_series'
-CONFIRMED = os.path.join(DATA_DIR, "time_series_19-covid-Confirmed.csv")
-DEATHS = os.path.join(DATA_DIR, "time_series_19-covid-Deaths.csv")
-RECOVERED = os.path.join(DATA_DIR, "time_series_19-covid-Recovered.csv")
+pd.set_option('display.max_colwidth', -1)
 
-confirmed_df = pd.read_csv(CONFIRMED)
-deaths_df = pd.read_csv(DEATHS)
-recovered_df = pd.read_csv(RECOVERED)
+df = load_data(type=time_series)
+
+data_type = streamlit.selectbox(label='Select data', options=['CONFIRMED', 'DEATHS', 'RECOVERED'], index=0)
+if data_type == 'CONFIRMED': 
+    response = 'confirmed'
+elif data_type == 'DEATHS': 
+    response = 'deaths'
+else: 
+    response = 'recovered'
+
+# Select Date
+current_date = df['date'].max()
+date = st.date_input("Select Date:", value=current_date)
