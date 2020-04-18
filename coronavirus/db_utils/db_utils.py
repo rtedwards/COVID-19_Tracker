bf7ac3877790a45f088dc516f2ba4dc9f53f9e0d
Robert <rtedwards.eng@gmail.com>
2020-04-18 14:47:32 +0100

    moved scripts into folder

--- coronavirus/db_utils/db_utils.py
+++ coronavirus/db_utils/db_utils.py
@@ -7,6 +7,37 @@ from pathlib import Path
 DATA_DIR = Path.cwd() / 'data'
 DEFAULT_PATH = DATA_DIR / 'database.sqlite3'
 
+def pull_data():
+    """
+    Pull data from John Hopkins github and store in DB (currently a csv)
+    """
+
+    # Should set these url paths as environment variables
+    CONFIRMED = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+    DEATHS = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
+    RECOVERED = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
+
+    confirmed_df = pd.read_csv(CONFIRMED)
+    deaths_df = pd.read_csv(DEATHS)
+    recovered_df = pd.read_csv(RECOVERED)
+
+    save_data(confirmed_df, DATA_DIR, "confirmed_jh.csv")
+    save_data(deaths_df, DATA_DIR, "deaths_jh.csv")
+    save_data(recovered_df, DATA_DIR, "recovered_jh.csv")
+
+
+def save_data(df, data_dir, filename):
+    """
+    Saves the data (to csv) in the data directory (creates directory if doesn't exit)
+        - Need to change to actual DB
+    :param data_dir: data directory
+    :param filename: name of saved file
+    """
+    if not Path(data_dir).is_dir():
+        data_dir.mkdir(parents=True, exist_ok=True)
+    df.to_csv(data_dir / filename)
+
+
 def db_connect(db_path=DEFAULT_PATH):
     data_dir = db_path.parent
     connection = None
@@ -25,6 +56,7 @@ def db_connect(db_path=DEFAULT_PATH):
     return connection
 
 def main():
+    pull_data()
     db_connect()
 
 if __name__ == '__main__':
