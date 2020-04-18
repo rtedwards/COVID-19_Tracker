2440f56382399fee6f33f7f9f2882931af02a4a2
Robert <rtedwards.eng@gmail.com>
2020-04-18 17:41:06 +0100

    class functions to acces DB

--- coronavirus/db_utils/db_utils.py
+++ coronavirus/db_utils/db_utils.py
@@ -7,8 +7,25 @@ from pathlib import Path
 # called 'database.sqlite3' in the same directory as this script
 class DataBase():
     def __init__(self, db_name):
+        self.db_name = db_name
         self.data_dir = Path.cwd() / 'data'
-        self.db_path = self.data_dir / db_name
+        self.db_path = self.data_dir / self.db_name
+        self.connection = self.connect()
+
+
+    def connect(self):
+        """Connects to SQLite DB"""
+        connection = None
+        try:
+            if not Path(self.data_dir).is_dir():
+                self.data_dir.mkdir(parents=True, exist_ok=True)
+
+            connection = sqlite3.connect(self.db_path)
+            print("Connection to SQLite DB successful")
+        except Error as e:
+            print(f"The error '{e}' occurred")
+        return connection
+
 
     def pull_data(self, url, name, csv=False):
         """
@@ -20,13 +37,18 @@ class DataBase():
         if csv:
             self.save_to_csv(df, name + ".csv")
 
+
     def save_to_db(self, df, table_name):
         """
         Saves the data to a table in the DB 
         :param df: pandas dataframe
         :param table_name: name of table to save data
+
+        ** Need to check / drop duplicates **
         """
-        df.to_sql(table_name, self.db_connection, if_exists='append', index=False)
+        df.to_sql(table_name, self.connection, if_exists='append', index=False)
+        print(f"> Inserted {table_name} into {self.db_name}")
+
 
     def save_to_csv(self, df, file_name):
         """
@@ -40,20 +62,39 @@ class DataBase():
         df.to_csv(self.data_dir / file_name)
 
 
-    def connect(self):
-        """Connects to SQLite DB"""
-        connection = None
+    def execute_query(self, query):
+        self.connection.autocommit = True
+        cursor = self.connection.cursor()
         try:
-            if not Path(self.data_dir).is_dir():
-                self.data_dir.mkdir(parents=True, exist_ok=True)
+            cursor.execute(query)
+            print("Query executed successfully")
+        except OperationalError as e:
+            print(f"The error '{e}' occurred")
 
-            connection = sqlite3.connect(self.db_path)
-            print("Connection to SQLite DB successful")
 
+    def execute_read_query(self, query):
+        cursor = self.connection.cursor()
+        result = None
+        try:
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return result
         except Error as e:
             print(f"The error '{e}' occurred")
 
-        self.db_connection = connection
+
+    def read_table_to_dataframe(self, table_name):
+        return pd.read_sql_query("SELECT * FROM " + table_name, self.connection)
+
+
+    def list_tables(self):
+        cursor = self.connection.cursor()
+        # cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        cursor.execute("SELECT * FROM sqlite_master")
+        tables = cursor.fetchall()
+        tables = [x[2] for x in tables]
+        print(f"> Tables in {self.db_name}: {tables}")
+
 
 def main():
     # Should set these url paths as environment variables
@@ -61,11 +102,16 @@ def main():
     DEATHS = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
     RECOVERED = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
 
-    db = DataBase('database.sqlite3')
-    db_connection = db.connect()
+    # db = DataBase('database.sqlite3')
+    db = DataBase('COVID-19.db')
     db.pull_data(url=CONFIRMED, name='jh_confirmed', csv=True)
     db.pull_data(url=DEATHS, name='jh_deaths', csv=True)
     db.pull_data(url=RECOVERED, name='jh_recovered', csv=True)
+
+    db.list_tables()
+    df = db.read_table_to_dataframe('jh_deaths')
+    print(df.head())
+
 
 if __name__ == '__main__':
     main()
