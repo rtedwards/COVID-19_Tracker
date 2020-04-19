16e45489dc99576405e465500f71976d073813f2
Robert <rtedwards.eng@gmail.com>
2020-04-19 11:28:19 +0100

    updated choropleth function so it might run

--- coronavirus/mapper/mapper.py
+++ coronavirus/mapper/mapper.py
@@ -6,6 +6,7 @@ import os
 import pandas as pd
 import urllib
 
+
 def choropleth_map(df,columns,geo_data,color,legend):
     cmap = folium.Map(location=[48, -102], 
                       tiles='OpenStreetMap',
@@ -13,15 +14,16 @@ def choropleth_map(df,columns,geo_data,color,legend):
                       min_zoom=4,
                       max_zoom=15)
 
-    folium.Choropleth(geo_data=country_geo,
+    folium.Choropleth(geo_data=geo_data,
                       name='choropleth',
                       data=df,
-                      columns=['ISO3 Code', 'confirmed'],
+                    #   columns=['ISO3 Code', 'confirmed'],
+                      columns=columns,
                       key_on='feature.id',
-                      fill_color='YlGn',
+                      fill_color=color,
                       fill_opacity=0.7,
                       line_opacity=0.2,
-                      legend_name='Confirmed Cases',
+                      legend_name=legend,
                       show=False
     ).add_to(cmap)
 
