d6a4b2de9b447e6880aee0aee0f07d20af630be9
Robert <rtedwards.eng@gmail.com>
2020-04-19 11:28:39 +0100

    Now just has preprocessing helper functions

--- coronavirus/preprocessor/preprocessor.py
+++ coronavirus/preprocessor/preprocessor.py
@@ -5,48 +5,30 @@ import json
 import folium
 import urllib
 import os
-import coronavirus.db_utils.db_utils as dbu
-import coronavirus.preprocessor.preprocessor
 
-def load_data(type=time_series): 
+def convert_jh_global_time_series_to_long(df, name): 
+    """Converts JH global time series data from wide to long format"""
+    df = df.melt(id_vars=['Province/State', 'Country/Region', 'Lat', 'Long'],
+                 var_name='date', 
+                 value_name=name)
 
-    if type == time_series:
-        DATA_DIR = Path.cwd() / 'data'
+    # Convert to datetime
+    df['date'] = pd.to_datetime(df['date'], format="%m/%d/%y")
 
-        confirmed_df = pd.read_csv(CONFIRMED)
-        deaths_df = pd.read_csv(DEATHS)
-        recovered_df = pd.read_csv(RECOVERED)
+    # Rename columns
+    df.columns = ['province/state', 'country/region', 'latitude', 'longitude', 'date', name]
+    return df
 
-        # Convert from Wide to Long format
-        confirmed_df = confirmed_df.melt(id_vars=['Province/State', 'Country/Region', 'Lat', 'Long'],
-                                         var_name='date', 
-                                         value_name='confirmed')
-        deaths_df = deaths_df.melt(id_vars=['Province/State', 'Country/Region', 'Lat', 'Long'],
-                                   var_name='date', 
-                                   value_name='deaths')
-        recovered_df = recovered_df.melt(id_vars=['Province/State', 'Country/Region', 'Lat', 'Long'],
-                                         var_name='date', 
-                                         value_name='recovered')
-        # Merge data frames
-        df = pd.merge(confirmed_df, deaths_df, 
-                      on=['Province/State', 'Country/Region', 'Lat', 'Long', 'date'],
-                      how='inner')
-        df = pd.merge(df, recovered_df, 
-                      on=['Province/State', 'Country/Region', 'Lat', 'Long', 'date'],
-                      how='inner')
 
-        # Convert to datetime
-        df['date'] = pd.to_datetime(df['date'], format="%m/%d/%y")
+def merge_dataframes(df1, df2, df3=None):
+    """Merges JH global time series dataframes"""
+    merged_df = pd.merge(df1, df1, 
+                    on=['Province/State', 'Country/Region', 'Lat', 'Long', 'date'],
+                    how='inner')
+    
+    if df3:
+        merged_df = pd.merge(merged_df, df3, 
+                    on=['Province/State', 'Country/Region', 'Lat', 'Long', 'date'],
+                    how='inner')
 
-        return df
-
-    elif type == daily:
-        DAILY_DATA_DIR = '/Users/Berto/Projects/COVID-19_Tracker/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports'
-
-        date_list = os.listdir(DAILY_DATA_DIR)
-        date_list.remove('.gitignore')
-        date_list.remove('README.md')
-        date_list.sort()
-
-    else:
-        print("TypeError")
+    return merged_df
